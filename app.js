38512ec2b346b72f61bc56c92af5fda9ce7e8438
TimamBinSaifTahmid <timam@iut-dhaka.edu>
2021-12-08 12:15:45 +0600

    3rd commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello world");
+  res.send("Hello world IT is ci cd lab test");
 });
 app.listen(3000, () => {
   console.log("asd");
